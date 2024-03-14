93057f1cae0e641b122346696e9dc70b6276717c
tongyan.zjy <tongyan.zjy@alibaba-inc.com>
2024-03-14 11:24:51 +0800

    zhujiayan

--- examples/bge_m3_test/evaluate_model_m3.sh
+++ examples/bge_m3_test/evaluate_model_m3.sh
@@ -1,5 +1,5 @@
 mkdir eval_msmarco_m3
-python -m FlagEmbedding.baai_general_embedding.finetune.eval_msmarco_m3 \
+python -m FlagEmbedding.baai_general_embedding.finetune.eval_msmarco_bgem3 \
 --encoder gbe-m3-toy-finetune \
 --fp16 \
 --save_embedding \
